bbf5cd566eb7daa5f70509f63bf9dd5ab5a29966
AISHWARYA <50246502+aishwarya-modi@users.noreply.github.com>
2020-10-20 22:26:13 +0530

    ping ball

--- main.py
+++ main.py
@@ -1,167 +1,253 @@
 import pygame
+import sys
+import os
 
-BLACK = (0, 0, 0)
+'''
+Variables
+'''
 
-WHITE = (255, 255, 255)
+worldx = 960
+worldy = 720
+fps = 40
+ani = 4
+world = pygame.display.set_mode([worldx, worldy])
 
-RED = (255, 0, 0)
+BLUE = (25, 25, 200)
+BLACK = (23, 23, 23)
+WHITE = (254, 254, 254)
+ALPHA = (0, 255, 0)
 
-GREEN = (0, 255, 0)
+'''
+Objects
+'''
 
-BLUE = (0, 0, 255)
+# x location, y location, img width, img height, img file
+class Platform(pygame.sprite.Sprite):
+    def __init__(self, xloc, yloc, imgw, imgh, img):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(os.path.join('images', img)).convert()
+        self.image.convert_alpha()
+        self.image.set_colorkey(ALPHA)
+        self.rect = self.image.get_rect()
+        self.rect.y = yloc
+        self.rect.x = xloc
 
-pygame.init()
+class Player(pygame.sprite.Sprite):
+    """
+    Spawn a player
+    """
 
-# Initializing the display window
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.movex = 0
+        self.movey = 0
+        self.frame = 0
+        self.health = 10
+        self.images = []
+        for i in range(1, 5):
+            img = pygame.image.load(os.path.join('images', 'hero' + str(i) + '.png')).convert()
+            img.convert_alpha()
+            img.set_colorkey(ALPHA)
+            self.images.append(img)
+            self.image = self.images[0]
+            self.rect = self.image.get_rect()
 
-size = (800, 600)
+    def gravity(self):
+        self.movey += 3.2
+        if self.rect.y > worldy and self.movey >= 0:
+            self.movey = 0
+            self.rect.y = worldy-ty-ty
 
-screen = pygame.display.set_mode(size)
+    def control(self, x, y):
+        """
+        control player movement
+        """
+        self.movex += x
+        self.movey += y
 
-pygame.display.set_caption("pong")
+    def update(self):
+        """
+        Update sprite position
+        """
 
-# Starting coordinates of the paddle
+        self.rect.x = self.rect.x + self.movex
+        self.rect.y = self.rect.y + self.movey
 
-rect_x = 400
+        # moving left
+        if self.movex < 0:
+            self.frame += 1
+            if self.frame > 3 * ani:
+                self.frame = 0
+            self.image = pygame.transform.flip(self.images[self.frame // ani], True, False)
 
-rect_y = 580
+        # moving right
+        if self.movex > 0:
+            self.frame += 1
+            if self.frame > 3 * ani:
+                self.frame = 0
+            self.image = self.images[self.frame // ani]
 
-# initial speed of the paddle
-
-rect_change_x = 0
-
-rect_change_y = 0
-
-# initial position of the ball
-
-ball_x = 50
-
-ball_y = 50
-
-# speed of the ball
-
-ball_change_x = 5
-
-ball_change_y = 5
-
-score = 0
-
-
-# draws the paddle. Also restricts its movement between the edges
-
-# of the window.
-
-def drawrect(screen, x, y):
-    if x <= 0:
-        x = 0
-
-    if x >= 699:
-        x = 699
-
-    pygame.draw.rect(screen, RED, [x, y, 100, 20])
+        hit_list = pygame.sprite.spritecollide(self, enemy_list, False)
+        for enemy in hit_list:
+            self.health -= 1
+            print(self.health)
 
 
-# game's main loop
+class Enemy(pygame.sprite.Sprite):
+    """
+    Spawn an enemy
+    """
 
-done = False
+    def __init__(self, x, y, img):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(os.path.join('images', img))
+        self.image.convert_alpha()
+        self.image.set_colorkey(ALPHA)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.counter = 0
 
+    def move(self):
+        """
+        enemy movement
+        """
+        distance = 80
+        speed = 8
+
+        if self.counter >= 0 and self.counter <= distance:
+            self.rect.x += speed
+        elif self.counter >= distance and self.counter <= distance * 2:
+            self.rect.x -= speed
+        else:
+            self.counter = 0
+
+        self.counter += 1
+
+
+class Level:
+    def ground(lvl, gloc, tx, ty):
+        ground_list = pygame.sprite.Group()
+        i = 0
+        if lvl == 1:
+            while i < len(gloc):
+                ground = Platform(gloc[i], worldy - ty, tx, ty, 'tile-ground.png')
+                ground_list.add(ground)
+                i = i + 1
+
+        if lvl == 2:
+            print("Level " + str(lvl))
+
+        return ground_list
+
+    def bad(lvl, eloc):
+        if lvl == 1:
+            enemy = Enemy(eloc[0], eloc[1], 'enemy.png')
+            enemy_list = pygame.sprite.Group()
+            enemy_list.add(enemy)
+        if lvl == 2:
+            print("Level " + str(lvl))
+
+        return enemy_list
+
+    # x location, y location, img width, img height, img file
+    def platform(lvl, tx, ty):
+        plat_list = pygame.sprite.Group()
+        ploc = []
+        i = 0
+        if lvl == 1:
+            ploc.append((200, worldy - ty - 128, 3))
+            ploc.append((300, worldy - ty - 256, 3))
+            ploc.append((500, worldy - ty - 128, 4))
+            while i < len(ploc):
+                j = 0
+                while j <= ploc[i][2]:
+                    plat = Platform((ploc[i][0] + (j * tx)), ploc[i][1], tx, ty, 'tile.png')
+                    plat_list.add(plat)
+                    j = j + 1
+                print('run' + str(i) + str(ploc[i]))
+                i = i + 1
+
+        if lvl == 2:
+            print("Level " + str(lvl))
+
+        return plat_list
+
+
+'''
+Setup
+'''
+
+backdrop = pygame.image.load(os.path.join('images', 'stage.png'))
 clock = pygame.time.Clock()
+pygame.init()
+backdropbox = world.get_rect()
+main = True
 
-while not done:
+player = Player()  # spawn player
+player.rect.x = 0  # go to x
+player.rect.y = 30  # go to y
+player_list = pygame.sprite.Group()
+player_list.add(player)
+steps = 10
 
+eloc = []
+eloc = [300, 0]
+enemy_list = Level.bad(1, eloc)
+
+gloc = []
+tx = 64
+ty = 64
+
+i = 0
+while i <= (worldx / tx) + tx:
+    gloc.append(i * tx)
+    i = i + 1
+
+ground_list = Level.ground(1, gloc, tx, ty)
+plat_list = Level.platform(1, tx, ty)
+
+'''
+Main Loop
+'''
+
+while main:
     for event in pygame.event.get():
-
         if event.type == pygame.QUIT:
+            pygame.quit()
+            try:
+                sys.exit()
+            finally:
+                main = False
 
-            done = True
+        if event.type == pygame.KEYDOWN:
+            if event.key == ord('q'):
+                pygame.quit()
+                try:
+                    sys.exit()
+                finally:
+                    main = False
+            if event.key == pygame.K_LEFT or event.key == ord('a'):
+                player.control(-steps, 0)
+            if event.key == pygame.K_RIGHT or event.key == ord('d'):
+                player.control(steps, 0)
+            if event.key == pygame.K_UP or event.key == ord('w'):
+                print('jump')
 
-        elif event.type == pygame.KEYDOWN:
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == ord('a'):
+                player.control(steps, 0)
+            if event.key == pygame.K_RIGHT or event.key == ord('d'):
+                player.control(-steps, 0)
 
-            if event.key == pygame.K_LEFT:
-
-                rect_change_x = -6
-
-            elif event.key == pygame.K_RIGHT:
-
-                rect_change_x = 6
-
-            # elif event.key == pygame.K_UP:
-
-            # rect_change_y = -6
-
-            # elif event.key == pygame.K_DOWN:
-
-            # rect_change_y = 6'''
-
-        elif event.type == pygame.KEYUP:
-
-            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-
-                rect_change_x = 0
-
-            elif event.key == pygame.K_UP or event.key == pygame.K_DOWN:
-
-                rect_change_y = 0
-
-    screen.fill(BLACK)
-
-    rect_x += rect_change_x
-
-    rect_y += rect_change_y
-
-    ball_x += ball_change_x
-
-    ball_y += ball_change_y
-
-    # this handles the movement of the ball.
-
-    if ball_x < 0:
-
-        ball_x = 0
-
-        ball_change_x = ball_change_x * -1
-
-    elif ball_x > 785:
-
-        ball_x = 785
-
-        ball_change_x = ball_change_x * -1
-
-    elif ball_y < 0:
-
-        ball_y = 0
-
-        ball_change_y = ball_change_y * -1
-
-    elif ball_x > rect_x and ball_x < rect_x + 100 and ball_y == 565:
-
-        ball_change_y = ball_change_y * -1
-
-        score = score + 1
-
-    elif ball_y > 600:
-
-        ball_change_y = ball_change_y * -1
-
-        score = 0
-
-    pygame.draw.rect(screen, WHITE, [ball_x, ball_y, 15, 15])
-
-    # drawball(screen,ball_x,ball_y)
-
-    drawrect(screen, rect_x, rect_y)
-
-    # score board
-
-    font = pygame.font.SysFont('Calibri', 15, False, False)
-
-    text = font.render("Score = " + str(score), True, WHITE)
-
-    screen.blit(text, [600, 100])
-
+    world.blit(backdrop, backdropbox)
+    player.gravity()
+    player.update()
+    player_list.draw(world)
+    enemy_list.draw(world)
+    ground_list.draw(world)
+    plat_list.draw(world)
+    for e in enemy_list:
+        e.move()
     pygame.display.flip()
-
-    clock.tick(60)
-
-pygame.quit()
-
+    clock.tick(fps)
